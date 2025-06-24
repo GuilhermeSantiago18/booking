cabb4470bc728a03d5097c9a04800e8df07e45d6
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-24 09:36:45 -0300

    feat: app and server js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const app = require('./app');
+const sequelize = require('./config/database');
+
+const PORT = process.env.PORT || 3001;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection Success');
+
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server connected http://localhost:${PORT}`);
+    });
+
+  } catch (error) {
+    console.error('Error to connect no Database', error);
+  }
+})();
