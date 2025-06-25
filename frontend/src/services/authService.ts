43d910ce248ddaa983334d71ce7df880b85135a9
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-25 00:36:16 -0300

    fix: type registerPayload

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -6,24 +6,26 @@ interface LoginPayload {
 }
 
 interface RegisterPayload {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
   cep: string;
   state: string;
   city: string;
-  neighborhood: string;
+  district: string;
   street: string;
   number?: string;
   complement?: string;
+  role: string
 }
 
 export async function login(data: LoginPayload) {
-  const response = await api.post('/users/login', data);
+  const response = await api.post('/v1/users/login', data);
   return response.data;
 }
 
 export async function register(data: RegisterPayload) {
-  const response = await api.post('/users/register', data);
+  const response = await api.post('/v1/users/register', data);
   return response.data;
 }
