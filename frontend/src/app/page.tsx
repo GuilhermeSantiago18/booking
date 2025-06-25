f089178efe1aad2acaa79aa7c05f140475599dfd
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-25 00:37:22 -0300

    feat: add loading

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Loading from '@/components/Loading';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -17,7 +18,7 @@ export default function HomePage() {
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <p>Carregando...</p>
+      <Loading size={24}/>
     </div>
   );
 }
