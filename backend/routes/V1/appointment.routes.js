44e21a1d2317b24f9173a44db817ece89fe939a4
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-26 13:42:39 -0300

    feat: new route appointments create and get

---
+++ backend/routes/V1/appointment.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const AppointmentController = require('../../controllers/AppointmentController');
+const adminOnly = require('../../middlewares/AdminOnly');
+
+router.get('/', AppointmentController.getAll);
+router.post('/', AppointmentController.createAppointment);
+
+module.exports = router;
