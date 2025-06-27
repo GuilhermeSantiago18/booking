3f10c4db459a15f6d548172910b58446dd9ef173
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-27 17:21:18 -0300

    fix types register

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,31 +1,19 @@
 import { api } from './api';
+import { IUserFormData } from '@/types/User';
+
 
 interface LoginPayload {
   email: string;
   password: string;
 }
 
-interface RegisterPayload {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  postalCode: string;
-  state: string;
-  city: string;
-  district: string;
-  street: string;
-  number?: string;
-  complement?: string;
-  role: string
-}
 
 export async function login(data: LoginPayload) {
   const response = await api.post('/users/login', data);
   return response;
 }
 
-export async function register(data: RegisterPayload) {
+export async function register(data: IUserFormData) {
   const response = await api.post('/users/register', data);
   return response;
 }
