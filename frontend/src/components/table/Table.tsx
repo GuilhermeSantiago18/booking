ddb55459d6e4987d77f2441d6db6b3fb92a5b9e1
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-27 11:32:08 -0300

    style: adjust for empty results table

--- frontend/src/components/table/Table.tsx
+++ frontend/src/components/table/Table.tsx
@@ -14,50 +14,63 @@ interface TableProps<T> {
   getRowClassName?: (row: T) => string;
 }
 
+export default function Table<T>({
+  headers,
+  data,
+  renderActions,
+  getRowClassName,
+}: TableProps<T>) {
+  if (data.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center  md:h-[calc(70vh)] p-4">
+        <Image
+          src="/assets/Dashboard.svg"
+          alt="Dashboard"
+          width={200}
+          height={200}
+          className="mb-4"
+        />
+        <p className="font-medium text-center text-lg font-montserrat">Nada por aqui ainda...</p>
+      </div>
+    );
+  }
 
-
-
-
-export default function Table<T>({ headers, data, renderActions, getRowClassName }: TableProps<T>) {
   return (
     <div className="overflow-x-auto bg-white rounded shadow-md font-montserrat">
       <table className="min-w-full table-auto border">
         <thead className="bg-gray-100">
           <tr>
             {headers.map((header) => (
-              <th key={String(header.key)} className="text-left px-4 py-2 border-b">{header.label}</th>
+              <th
+                key={String(header.key)}
+                className="text-left px-4 py-2 border-b"
+              >
+                {header.label}
+              </th>
             ))}
-            {renderActions && <th className="px-4 py-2 border-b">Ações</th>}
+            {renderActions && (
+              <th className="px-4 py-2 border-b">
+                Ação
+              </th>
+            )}
           </tr>
         </thead>
         <tbody>
-          {data.length === 0 ? (
-            <tr>
-              <td colSpan={headers.length + (renderActions ? 1 : 0)} className="text-center py-4">
-                <div className="flex justify-center mt-34">
-                       <Image
-                         src="/assets/Dashboard.svg"
-                         alt="Dashboard"
-                         width={200}
-                         height={200}
-                       />
-                     </div>
-              </td>
+          {data.map((row, idx) => (
+            <tr
+              key={idx}
+              className={getRowClassName ? getRowClassName(row) : "bg-white"}
+            >
+              {headers.map((header) => (
+                <td key={String(header.key)} className="px-4 py-2 border-b">
+                  {String(row[header.key])}
+                </td>
+              ))}
+              {renderActions && (
+                <td className="px-4 py-2 border-b">{renderActions(row)}</td>
+              )}
             </tr>
-          ) : (
-            data.map((row, idx) => (
-              <tr key={idx}   className={getRowClassName ? getRowClassName(row) : 'bg-white'}>
-                {headers.map((header) => (
-                  <td key={String(header.key)} className="px-4 py-2 border-b">{String(row[header.key])}</td>
-                ))}
-                {renderActions && (
-                  <td className="px-4 py-2 border-b">
-                    {renderActions(row)}
-                  </td>
-                )}
-              </tr>
-            ))
-          )}
+          ))}
         </tbody>
       </table>
     </div>
