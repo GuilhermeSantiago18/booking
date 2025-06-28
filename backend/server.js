ab5ff154a2747614e4500979ac1d9e26775a377a
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-28 10:56:55 -0300

    fix: envs backend for deploy

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,21 @@ const PORT = process.env.PORT || 3001;
 
 (async () => {
   try {
+
+    console.log('Connecting to DB with:', {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
     await sequelize.authenticate();
     console.log('Connection Success');
 
     await sequelize.sync();
 
     app.listen(PORT, () => {
-      console.log(`Server connected http://localhost:${PORT}`);
+      console.log(`Server connected on PORT:${PORT}`);
     });
   } catch (error) {
     console.error('Error to connect no Database', error);
