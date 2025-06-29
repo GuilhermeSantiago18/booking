7cfade61c340d090597809a87f2c245d7a91de42
Guilherme <guilherme.limasantiago@gmail.com>
2025-06-29 13:08:30 -0300

    feat: add middleware to register logs update user

--- backend/routes/V1/user.routes.js
+++ backend/routes/V1/user.routes.js
@@ -8,6 +8,6 @@ router.post('/register', UserController.register);
 router.post('/register/admin', UserController.registerAdmin);
 router.post('/login', UserController.login);
 router.get('/me', authMiddleware, UserController.getUserByID);
-router.put('/me', authMiddleware, logActionMiddleware({type: 'Atualização de dados pessoais', module: 'Perfil'}),  UserController.updateUser);
+router.put('/me', authMiddleware, logActionMiddleware({type: 'Atualização de dados pessoais', module: 'Minha conta'}),  UserController.updateUser);
 
 module.exports = router;
